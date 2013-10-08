876eac5da01287a94f4a0699f1f3c9c79baeb9fa
XingHui Jin <kim4coder@gmail.com>
2013-10-08 13:09:01 +0800

    TEST
    TEST1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Globalization;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = System.IO.File.ReadAllText("D:/SKLAB/SKLAB.Admin.UI/LSY040.cs");
+            System.IO.File.WriteAllBytes("D:/1.xml", Encoding.UTF8.GetBytes(str));
+            string pattern = Regex.Escape("\"") + "(.*?)\"";
+            MatchCollection matches = Regex.Matches(str, pattern);
+            int commentNumber = 0;
+
+            System.Text.Encoding originalEncoding = System.Text.Encoding.GetEncoding(1252);
+            System.Text.Encoding euckr = System.Text.Encoding.GetEncoding(949);
+
+            foreach (Match match in matches)
+            {
+                //Console.OutputEncoding = Encoding.GetEncoding("EUC-KR");
+
+                byte[] rawbytes = originalEncoding.GetBytes(match.Value.ToString());
+                string s = euckr.GetString(rawbytes);
+                Console.WriteLine("   {0}: {1}", ++commentNumber, s);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
